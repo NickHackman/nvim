510aa0dda07e16ed18745ae6745b2cbda5d7077b
Nick Hackman <snickhackman@gmail.com>
2023-07-04 15:48:30 -0400

    style(keymaps): format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,23 +4,18 @@ local wk = require('which-key')
 
 -- Reigster keymaps in which-key
 wk.register({
-    ["<leader>"] = {
-        w = {
-            name = 'Window'
-        },
-        b = {
-            name = 'Buffer'
-        },
-        o = {
-            name = 'Open'
-        }
+    ['<leader>'] = {
+        w = { name = 'Window' },
+        b = { name = 'Buffer' },
+        o = { name = 'Open' },
+        d = { name = 'Diagnostics' }
     }
 })
 
 
 --- Creates options to provide to {vim.keymap.set}.
 -- adds the default options of { noremap = true, silent = true }
----@param custom_opts table | nil custom options like "desc"
+---@param custom_opts table | nil custom options like 'desc'
 ---@return table options combined with the default
 local opts = function(custom_opts)
     -- https://hiphish.github.io/blog/2020/12/31/spreading-tables-in-lua/
@@ -53,39 +48,43 @@ keymap.set('n', '<leader><leader>', telescope.find_files, opts { desc = 'Search 
 
 
 -- <leader>[w]indow
-keymap.set('n', '<leader>wv', '<c-w>v', opts { desc = "[W]indow [V]ertical Split" })
-keymap.set('n', '<leader>ws', '<c-w>s', opts { desc = "[Window] [S]plit" })
-keymap.set('n', '<leader>wl', '<c-w>l', opts { desc = "[W]indow Right" })
-keymap.set('n', '<leader>wh', '<c-w>h', opts { desc = "[W]indow Left" })
-keymap.set('n', '<leader>wj', '<c-w>j', opts { desc = "[W]indow Down" })
-keymap.set('n', '<leader>wk', '<c-w>k', opts { desc = "[W]indow Up" })
-keymap.set('n', '<leader>wq', ':wq<CR>', opts { desc = "[W]indow [Q]uit" })
+keymap.set('n', '<leader>wv', '<c-w>v', opts { desc = '[W]indow [V]ertical Split' })
+keymap.set('n', '<leader>ws', '<c-w>s', opts { desc = '[Window] [S]plit' })
+keymap.set('n', '<leader>wl', '<c-w>l', opts { desc = '[W]indow Right' })
+keymap.set('n', '<leader>wh', '<c-w>h', opts { desc = '[W]indow Left' })
+keymap.set('n', '<leader>wj', '<c-w>j', opts { desc = '[W]indow Down' })
+keymap.set('n', '<leader>wk', '<c-w>k', opts { desc = '[W]indow Up' })
+keymap.set('n', '<leader>wq', ':wq<CR>', opts { desc = '[W]indow [Q]uit' })
+keymap.set('n', '<leader>wH', ':vertical resize +5<CR>', opts { desc = '[W]indow Resize Left' })
+keymap.set('n', '<leader>wL', ':vertical resize -5<CR>', opts { desc = '[W]indow Resize Right' })
+keymap.set('n', '<leader>wJ', ':resize -5<CR>', opts { desc = '[W]indow Resize Down' })
+keymap.set('n', '<leader>wK', ':resize +5<CR>', opts { desc = '[W]indow Resize Up' })
 
 -- <leader>[b]uffer
 keymap.set('n', '<leader>bs', telescope.buffers, opts { desc = '[B]uffer [S]earch' })
-keymap.set('n', '<leader>bn', ':bnext<CR>', opts { desc = "[B]uffer [N]ext" })
-keymap.set('n', '<leader>bp', ':bprev<CR>', opts { desc = "[B]uffer [P]revious" })
+keymap.set('n', '<leader>bn', ':bnext<CR>', opts { desc = '[B]uffer [N]ext' })
+keymap.set('n', '<leader>bp', ':bprev<CR>', opts { desc = '[B]uffer [P]revious' })
 
--- <leader>e
-keymap.set("n", "<leader>dt", "<cmd>TroubleToggle<cr>", opts { desc = '[D]iagnostics [T]oggle' })
-keymap.set("n", "<leader>dw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts { desc = '[D]iagnostics [W]orkspace' })
-keymap.set("n", "<leader>ed", "<cmd>TroubleToggle document_diagnostics<cr>", opts { desc = '[D]iagnostics [D]ocument' })
-keymap.set("n", "<leader>dl", "<cmd>TroubleToggle loclist<cr>", opts { desc = '[D]iagnostics [L]ocal' })
-keymap.set("n", "<leader>dq", "<cmd>TroubleToggle quickfix<cr>", opts { desc = '[D]iagnostics [Q]uickfix' })
+-- <leader>d
+keymap.set('n', '<leader>dt', '<cmd>TroubleToggle<cr>', opts { desc = '[D]iagnostics [T]oggle' })
+keymap.set('n', '<leader>dw', '<cmd>TroubleToggle workspace_diagnostics<cr>', opts { desc = '[D]iagnostics [W]orkspace' })
+keymap.set('n', '<leader>ed', '<cmd>TroubleToggle document_diagnostics<cr>', opts { desc = '[D]iagnostics [D]ocument' })
+keymap.set('n', '<leader>dl', '<cmd>TroubleToggle loclist<cr>', opts { desc = '[D]iagnostics [L]ocal' })
+keymap.set('n', '<leader>dq', '<cmd>TroubleToggle quickfix<cr>', opts { desc = '[D]iagnostics [Q]uickfix' })
 
 -- <leader>[o]pen
-keymap.set('n', '<leader>op', ':Neotree toggle<CR>', opts { desc = "[O]pen [P]roject" })
-keymap.set('n', '<leader>ol', 'gx', opts { desc = "[O]pen [L]ink" })
-keymap.set('n', '<leader>og', ':Neogit<CR>', opts { desc = "[O]pen Ma[g]it" })
+keymap.set('n', '<leader>op', ':Neotree toggle<CR>', opts { desc = '[O]pen [P]roject' })
+keymap.set('n', '<leader>ol', 'gx', opts { desc = '[O]pen [L]ink' })
+keymap.set('n', '<leader>og', ':Neogit<CR>', opts { desc = '[O]pen Ma[g]it' })
 
 -- open terminal on the bottom with no line numbers
-keymap.set("n", "<leader>ot", "<cmd>sp<bar>term<cr><c-w>J:resize10<cr>:setlocal nonumber<cr>i",
-    opts { desc = "[O]pen [T]erminal" })
+keymap.set('n', '<leader>ot', '<cmd>sp<bar>term<cr><c-w>J:resize10<cr>:setlocal nonumber<cr>i',
+    opts { desc = '[O]pen [T]erminal' })
 
 -- Improved indentation
-keymap.set("v", "<", "<gv", opts {})
-keymap.set("v", ">", ">gv", opts {})
+keymap.set('v', '<', '<gv', opts {})
+keymap.set('v', '>', '>gv', opts {})
 
 -- Center search results
-keymap.set("n", "n", "nzz", opts {})
-keymap.set("n", "N", "Nzz", opts {})
+keymap.set('n', 'n', 'nzz', opts {})
+keymap.set('n', 'N', 'Nzz', opts {})
