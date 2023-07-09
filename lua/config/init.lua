a2f6d18c9b8678825e09bc2a9370f833087b5810
Nick Hackman <snickhackman@gmail.com>
2023-07-08 22:08:32 -0400

    feat(options): use ripgrep over grep
    If ripgrep is installed use it over grep to improve performance of
    search.

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,16 +5,22 @@ local opt = vim.opt
 -------------------------------------------
 -- General
 -------------------------------------------
-opt.number = true                             -- show line numbers
-opt.mouse = "a"                               -- enable mouse
-opt.swapfile = false                          -- disable vim swapfiles
+opt.number = true -- show line numbers
+opt.mouse = "a" -- enable mouse
+opt.swapfile = false -- disable vim swapfiles
 opt.completeopt = "menuone,noinsert,noselect" -- better autocomplete
-opt.clipboard = "unnamedplus"                 -- Sync clipboard between OS and Neovim.
-opt.termguicolors = true                      -- 24-bit color
-opt.showmatch = true                          -- show matching paren
-opt.undofile = true                           -- undofile for use with undotree
+opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
+opt.termguicolors = true -- 24-bit color
+opt.showmatch = true -- show matching paren
+opt.undofile = true -- undofile for use with undotree
 opt.signcolumn = "yes"
 opt.relativenumber = true
+
+-- Use https://github.com/BurntSushi/ripgrep over grep
+if vim.fn.executable("rg") == 1 then
+	opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+	opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+end
 
 -------------------------------------------
 -- Case Insensitive Search
