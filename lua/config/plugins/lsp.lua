1a9afc5ba06ffd09d8ea3aeb13b59453375c9d55
Nick Hackman <snickhackman@gmail.com>
2025-06-18 17:47:56 -0400

    refactor: better variable name

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -21,21 +21,22 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-		local mason_servers = vim.deepcopy(servers)
+		local mason_installable_servers = vim.deepcopy(servers)
 		-- removes kotlin_lsp because that lsp isn't installable via Mason
-		mason_servers["kotlin_lsp"] = nil
+		mason_installable_servers["kotlin_lsp"] = nil
 
 		-- Ensure all LSPs are installed
 		mason_lspconfig.setup({
-			ensure_installed = vim.tbl_keys(mason_servers),
+			ensure_installed = vim.tbl_keys(mason_installable_servers),
 		})
 
 		for name, server in pairs(servers) do
 			vim.lsp.enable(name)
+
 			vim.lsp.config(name, {
 				capabilities = capabilities,
 				on_attach = require("config.keymaps").lsp_keybindings,
-				settings = server.settings or {}
+				settings = server.settings or {},
 			})
 		end
 	end,
